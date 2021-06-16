abb191f88c2f532b7e4b1d8c0ce91c807a3e41b2
dsanson <dsanson@companieshouse.gov.uk>
2021-06-16 17:31:10 +0100

    Added basic session display on html page ready for database plugin

--- app.js
+++ app.js
@@ -1,50 +1,46 @@
 const express = require('express');
+const path = require('path');
+
 const chalk = require('chalk');
 const debug = require('debug')('app');
 const morgan = require('morgan');
+
+const PORT = process.env.PORT || 3000;
 const sessions = require('./src/data/sessions.json');
 
 const app = express();
 const sessionsRouter = express.Router();
+app.use('/sessions', sessionsRouter); // routes everything that goes to sessions the item that comes afyer sessions is passed ito router function
 // express lets us leverage the sesion router to build our code
 
-const PORT = process.env.PORT || 3000;
-// everything that comes out of env - or just in case we run on 3000
-const path = require('path');
 
 app.use(morgan('tiny'));
 app.use(express.static(path.join(__dirname, 'public/')));
+// everything that comes out of env - or just in case we run on 3000
+
 // express getting the static files by looking in public directory, if none found it defaults back
 
-const productsRouter = express.Router();
-productsRouter.route('/products')
-  .get((req, res) => {
-    const response = { hello: 'This is my API' };
-    res.json(response);
-  });
-app.use('/api', productsRouter);
 
-app.set('views', './src/views/');
+app.set('views', './src/views');
 app.set('view engine', 'ejs');
 
-sessionsRouter.route('/')
-  .get((req, res) => {
-    res.render('sessions', {
-      sessions,
-    });
+sessionsRouter.route('/').get((req, res) => {
+  res.render('sessions', {
+    sessions,
   });
-
-sessionsRouter.route('/1')
-  .get((req, res) => {
-    res.render('hello single session');
-  });
-
-app.use('/sessions', sessionsRouter); // routes everything that goes to sessions with that directory
-
-app.get('/', (req, res) => {
-  res.render('index', { title: 'Welcome to the Online Store! ', data: [['a', 'b', 'c']] });
 });
 
+sessionsRouter.route('/:id').get((req, res) => {
+  const id = req.params.id;
+  res.render('session', {
+    session: sessions[id],
+  });
+});
+
+// sessionsRouter.route('/:id').get((req, res) => {
+//   const id = req.params.id;
+//   res.send('hello session' + id);
+// });
 app.listen(PORT, () => {
   // listens to port in nodemon conifg
   debug(`Running on port ${chalk.green(PORT)}`);
